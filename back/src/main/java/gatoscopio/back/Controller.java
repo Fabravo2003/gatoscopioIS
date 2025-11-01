9eb2a8df14a9b038d1a98bfc1e89d4c6e615f098
Fabravo2003 <fabian.bravo2201@alumnos.ubiobio.cl>
2025-11-01 20:44:00 -0300

    Creacion de controlador y modelos

--- back/src/main/java/gatoscopio/back/Controller.java
+++ back/src/main/java/gatoscopio/back/Controller.java
@@ -1,5 +1,9 @@
 package gatoscopio.back;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Controller {
-    
+
 }
---
+++ back/src/main/java/gatoscopio/back/model/Usuario.java
@@ -0,0 +1,46 @@
+package gatoscopio.back.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+    private String correo;
+    private String contrasena;
+
+    // Getters y Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+}
